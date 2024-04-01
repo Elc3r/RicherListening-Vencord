3ec4f71dece6f615f679e4cc2d0e655a4e40195e
Michal Vavřiňák <vavrinakm@gmail.com>
2024-04-01 22:35:21 +0200

    fork update

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@components/Link";
+import { Logger } from "@utils/Logger";
 import definePlugin from "@utils/types";
 import { Forms } from "@webpack/common";
 const appIds = [
@@ -12,19 +13,28 @@ const appIds = [
     "463151177836658699",   // PreMiD (YouTube Music)
     "802958833214423081"    // PreMiD (SoundCloud)
 ];
+const logger = new Logger("RicherListening");
 export default definePlugin({
     name: "RicherListening",
     description: "A simple plugin to allow other music clients to use popular Discord types such as \"Listening to\" type prefix to the user's rich presence when an applicable ID is found.",
     authors: [{
         id: 191621342473224192n,
         name: "cryptofyre",
-    },{
+    },
+    {
+        id: 546426958465073163n,
+        name: "Core",
+    },
+    {
         id: 540970385802526731n,
         name: "Elc3r",
     }],
+    start() {
+        logger.debug("Plugin started");
+    },
     patches: [
         {
-            find: '.displayName="LocalActivityStore"',
+            find: '="LocalActivityStore",',
             replacement: {
                 match: /LOCAL_ACTIVITY_UPDATE:function\((\i)\)\{/,
                 replace: "$&$self.patchActivity($1.activity);",
@@ -38,8 +48,9 @@ export default definePlugin({
             }
         }
     ],
-    patchActivity(activity: any) {
-        if (appIds.includes(activity.application_id)) {
+    patchActivity(activity: { application_id: string; type: number; }) {
+        if (appIds.includes(activity?.application_id)) {
+            logger.debug("Found a matching application ID, correcting activity type");
             activity.type = 2; /* LISTENING type */
         }
     },
